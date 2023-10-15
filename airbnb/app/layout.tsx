8e263082f0cf1aea749fe1da9f03fc298a15f787
shafiaansar <shafiaansar142@gmail.com>
2023-10-15 12:10:36 +0500

    Lets build this

---
+++ airbnb/app/layout.tsx
@@ -0,0 +1,20 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Nunito } from 'next/font/google'
+
+export const metadata: Metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb Clone',
+}
+const font = Nunito({
+  subsets:['latin']
+})
+export default function RootLayout({children,}:{
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  )
+}
