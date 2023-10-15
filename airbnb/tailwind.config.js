e8d2ba3ccd56416971722eb46f337096ef3df817
shafiaansar <shafiaansar142@gmail.com>
2023-10-15 12:23:38 +0500

    Set up

--- airbnb/tailwind.config.js
+++ airbnb/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+  './components/**/*.{js,ts,jsx,tsx,mdx}',
+  './app/**/*.{js,ts,jsx,tsx,mdx}',],
   theme: {
     extend: {},
   },
