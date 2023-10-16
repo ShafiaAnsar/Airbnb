ea7a0ba5593d6855be14f314d134c399355dc2b2
shafiaansar <shafiaansar142@gmail.com>
2023-10-16 12:52:38 +0500

    Lets start Building Navbar

---
+++ airbnb/app/components/Container.tsx
@@ -0,0 +1,10 @@
+interface ContainerProps{
+    children:React.ReactNode
+}
+const  Container:React.FC<ContainerProps> =({children}) =>{
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default Container
--- airbnb/app/layout.tsx
+++ airbnb/app/layout.tsx
@@ -1,20 +1,25 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Nunito } from 'next/font/google'
-
+import "./globals.css";
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import  Navbar from './components/Navbar/Navbar'
 export const metadata: Metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb Clone',
-}
+  title: "Airbnb",
+  description: "Airbnb Clone",
+};
 const font = Nunito({
-  subsets:['latin']
-})
-export default function RootLayout({children,}:{
-  children: React.ReactNode
+  subsets: ["latin"],
+});
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar/>
+        {children}
+        </body>
     </html>
-  )
+  );
 }
