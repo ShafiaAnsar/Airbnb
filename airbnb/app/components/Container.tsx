c8650988d48f66a2a648b1b2c2394dfa06d9a9b2
shafiaansar <shafiaansar142@gmail.com>
2023-10-17 11:48:04 +0500

    Logo and button's on Navbar

--- airbnb/app/components/Container.tsx
+++ airbnb/app/components/Container.tsx
@@ -1,9 +1,17 @@
+'use client'
+
 interface ContainerProps{
     children:React.ReactNode
 }
 const  Container:React.FC<ContainerProps> =({children}) =>{
   return (
-    <div>{children}</div>
+    <div className="max-w-[2520px]
+    mx-auto
+    xl:px-20 
+    md:px-10
+    sm:px-2
+    px-4" 
+    >{children}</div>
   )
 }
 
