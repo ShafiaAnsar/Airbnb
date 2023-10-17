b928dddb6978b966e547bb1683aeaf1b9fcf7903
shafiaansar <shafiaansar142@gmail.com>
2023-10-17 12:57:34 +0500

    Completed responsive Navbar

--- airbnb/app/components/Navbar/Navbar.tsx
+++ airbnb/app/components/Navbar/Navbar.tsx
@@ -1,6 +1,8 @@
+'use client'
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
+import UserMenu from "./UserMenu";
 const Navbar = () => {
   return(
   <div className="fixed w-full bg-white z-10 shadow-sm">
@@ -9,7 +11,7 @@ const Navbar = () => {
             <div className=" flex flex-row item-center justify-between gap-3 md:gap-0">
                 <Logo/>
                 <Search/>
-
+                <UserMenu/>
             </div>
         </Container>
     </div>
