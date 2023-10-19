fbf302ee6a34db917788aa7ad0d02716cd67fb26
shafiaansar <shafiaansar142@gmail.com>
2023-10-19 14:43:08 +0500

    Building prisma database

--- airbnb/app/components/modals/Modal.tsx
+++ airbnb/app/components/modals/Modal.tsx
@@ -119,7 +119,7 @@ const Modal:React.FC<ModalProps> = ({
                       onClick={handleSubmit}
                       />
                     </div>
-
+                    {footer}
                   </div>
                 </div>
 
