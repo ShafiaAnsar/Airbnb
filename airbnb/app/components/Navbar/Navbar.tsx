cc684f5babca2c34c23bded117268a7b88bc03e8
shafiaansar <shafiaansar142@gmail.com>
2023-10-22 21:06:05 +0500

    Categorries

--- airbnb/app/components/Navbar/Navbar.tsx
+++ airbnb/app/components/Navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
+import Categories from "./Categories";
 import { SafeUser } from "@/app/types";
 
 interface NavbarProps{
@@ -20,6 +21,7 @@ const Navbar:React.FC<NavbarProps> = ({currentUser}) => {
             </div>
         </Container>
     </div>
+    <Categories/>
     </div>
     )
 };
