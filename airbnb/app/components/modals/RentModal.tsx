6cf03858d0a03305016abc9bb0583d7611035120
shafiaansar <shafiaansar142@gmail.com>
2023-10-25 21:26:29 +0500

    Counter

--- airbnb/app/components/modals/RentModal.tsx
+++ airbnb/app/components/modals/RentModal.tsx
@@ -8,7 +8,8 @@ import { categories } from "../Navbar/Categories"
 import CategoryInput from "../inputs/CategoryInput"
 import { FieldValues,  useForm } from "react-hook-form"
 import CountrySelect from "../inputs/CountrySelect"
-import Map from "../Map"
+import dynamic from "next/dynamic"
+import Counter from "../inputs/Counter"
 
 
 enum STEPS {
@@ -54,6 +55,14 @@ const RentModal = () => {
     });
    const category = watch('category')
    const location = watch('location')
+
+   const guestCount = watch('guestCount')
+   const roomCount = watch('roomCount')
+   const bathroomCount = watch('bathroomCount')
+   const Map = useMemo(()=>dynamic(()=> import ('../Map'),{
+    ssr:false
+
+   }),[location])
    const setCustomValue = (id:string,value:any) =>{
     setValue(id,value,{
       shouldDirty:true,
@@ -113,6 +122,38 @@ const RentModal = () => {
         onChange={(value)=>setCustomValue('location',value)}
         />
         <Map center={location?.latlng}/>
+      </div>
+    )
+  }
+  if(step === STEPS.INFO){
+    bodyContent=(
+      <div className="flex flex-col gap-8">
+        <Heading
+        title="Share some basics about your place"
+        subtitle="What amenities do you have?"
+        />
+        <Counter
+        title="Guests"
+        subtitle="How many guests do you allow?"
+        value={guestCount}
+        onChange={(value)=> setCustomValue( 'guestCount' ,value)}
+        />
+        <hr/>
+        <Counter
+        title="Rooms"
+        subtitle="How many rooms do you have?"
+        value={roomCount}
+        onChange={(value)=> setCustomValue( 'roomCount' ,value)}
+        />
+        <hr/>
+        <Counter
+        title="Bathrooms"
+        subtitle="How many bathrooms do you have?"
+        value={bathroomCount}
+        onChange={(value)=> setCustomValue( 'bathroomCount' ,value)}
+        />
+
+
       </div>
     )
   }
