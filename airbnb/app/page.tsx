5fc5f34b7fb0f1e59c9bc2f3b6051925423f3b06
shafiaansar <shafiaansar142@gmail.com>
2023-10-28 12:07:52 +0500

    Fetching and displaying lists

--- airbnb/app/page.tsx
+++ airbnb/app/page.tsx
@@ -1,6 +1,35 @@
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings from "./actions/getListings";
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingsCard from "./components/listings/ListingsCard";
 
-export default function Home() {
+export default async function Home() {
+  const listings = await getListings()
+  const currentUser = await getCurrentUser()
+  if(listings.length === 0){
+    return <ClientOnly>
+      <EmptyState showReset/>
+    </ClientOnly>
+  }
   return (
-    <div className= " text-rose-500 text-2xl   text-center  font-semibold ">Hello Airbnb!</div>
+    <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+         {listings.map((listing:any)=>{
+          return (
+            <ListingsCard
+            currentUser={currentUser}
+            key={listing.id}
+            data={listing}
+
+            />
+          )
+          })}
+
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
