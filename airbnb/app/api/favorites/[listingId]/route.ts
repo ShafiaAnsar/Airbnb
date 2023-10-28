9b356636b6c9e27552b6431e4c10714762745d0e
shafiaansar <shafiaansar142@gmail.com>
2023-10-28 17:05:12 +0500

    add to favrt

--- airbnb/app/api/favorites/[listingId]/route.ts
+++ airbnb/app/api/favorites/[listingId]/route.ts
@@ -1,56 +1,72 @@
 import { NextResponse } from "next/server";
 
 import getCurrentUser from "@/app/actions/getCurrentUser";
-import prisma from '@/app/libs/prismadb'
+import prisma from "@/app/libs/prismadb";
 
 interface IParams {
-    listindId?:string
+  listingId?: string;
 }
 
-export  async function POSt(
-    request:Request,
-    {params}:{params:IParams}) {
-    const currentUser = await getCurrentUser()
-    if(!currentUser){
-        return NextResponse.error()
+export async function POST(
+  request: Request, 
+  { params }: { params: IParams }
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== 'string') {
+    throw new Error('Invalid ID');
+  }
+
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+  favoriteIds.push(listingId);
+
+  const user = await prisma.user.update({
+    where: {
+      id: currentUser.id
+    },
+    data: {
+      favoriteIds
     }
-    const {listindId} = params
-    if(!listindId || typeof listindId !==  'string'){
-        throw new Error("Invalid ID")
-    }
-    let favoriteIds = {...(currentUser.favoriteIds || [])}
-    favoriteIds.push(listindId)
-    const user = await prisma.user.update({
-        where:{
-            id:currentUser.id
-        },
-        data:{
-            favoriteIds:favoriteIds
-        }
-    })
-    return Response.json(user)
+  });
+
+  return NextResponse.json(user);
 }
 
 export async function DELETE(
-    request:Request, {params}:{params:IParams}
-    ) {
-        const currentUser = await getCurrentUser()
-        if(!currentUser){
-            return NextResponse.error()
-        }
-        const {listindId} = params
-        if(!listindId || typeof listindId !==  'string'){
-            throw new Error("Invalid ID")
-        }
-        let favoriteIds ={...(currentUser.favoriteIds || [])}
-        favoriteIds.filter((id)=>  id !== listindId )
-        const user = await prisma.user.update({
-            where:{
-                id:currentUser.id
-            },
-            data:{
-                favoriteIds:favoriteIds
-            }
-        })
-        return Response.json(user)
+  request: Request, 
+  { params }: { params: IParams }
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== 'string') {
+    throw new Error('Invalid ID');
+  }
+
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+  favoriteIds = favoriteIds.filter((id) => id !== listingId);
+
+  const user = await prisma.user.update({
+    where: {
+      id: currentUser.id
+    },
+    data: {
+      favoriteIds
+    }
+  });
+
+  return NextResponse.json(user);
 }
--- airbnb/app/components/HeartButton.tsx
+++ airbnb/app/components/HeartButton.tsx
@@ -2,6 +2,7 @@
 
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai"
 import { SafeUser } from "../types"
+import useFavorite from "../hooks/UseFavorite"
 
 interface HeartButtonProps{
     listingId:string
@@ -9,8 +10,9 @@ interface HeartButtonProps{
 }
 
 const HeartButton: React.FC<HeartButtonProps> = ({listingId,currentUser}) => {
-    const hasFavorited = false
-    const toggleFavorite = ()=>{}
+    const {hasFavorited,toggleFavorite} = useFavorite({
+      listingId,currentUser
+    })
   return (
     <div
     onClick={toggleFavorite}
