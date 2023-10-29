16cf27f9582b17d7385ae6bb5c6db6e3a977d665
shafiaansar <shafiaansar142@gmail.com>
2023-10-29 11:02:24 +0500

    /

--- airbnb/middleware.ts
+++ airbnb/middleware.ts
@@ -1,11 +1,10 @@
-export {default} from 'next-auth/middleware'
+export { default } from "next-auth/middleware"
 
-export const config={
-    matcher:[
-        '/trips',
-        '/properties',
-        'reservations',
-        '/favorites'
-    ]
-
-}
+export const config = { 
+  matcher: [
+    "/trips",
+    "/reservations",
+    "/properties",
+    "/favorites"
+  ]
+};
