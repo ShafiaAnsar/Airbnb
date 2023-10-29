27177c2353d517046dae52320767aad6ff66b7a8
shafiaansar <shafiaansar142@gmail.com>
2023-10-29 10:57:48 +0500

    ..

--- airbnb/app/api/listings/route.ts
+++ airbnb/app/api/listings/route.ts
@@ -40,7 +40,7 @@ export async function POST(
       roomCount,
       bathroomCount,
       guestCount,
-      locationValue: location.value,
+      locationValues: location.value,
       price: parseInt(price, 10),
       userId: currentUser.id
     }
