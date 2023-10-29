6ae62f4c73ba98e055e5e9dd08faef4110a1c32e
shafiaansar <shafiaansar142@gmail.com>
2023-10-29 10:47:51 +0500

    completed

--- airbnb/app/actions/getListings.ts
+++ airbnb/app/actions/getListings.ts
@@ -7,7 +7,7 @@ export interface IListingsParams {
   bathroomCount?: number;
   startDate?: string;
   endDate?: string;
-  locationValue?: string;
+  locationValues?: string;
   category?: string;
 }
 
@@ -20,7 +20,7 @@ export default async function getListings(
       roomCount, 
       guestCount, 
       bathroomCount, 
-      locationValue,
+      locationValues,
       startDate,
       endDate,
       category,
@@ -54,8 +54,8 @@ export default async function getListings(
       }
     }
 
-    if (locationValue) {
-      query.locationValue = locationValue;
+    if (locationValues) {
+      query.locationValues = locationValues;
     }
 
     if (startDate && endDate) {
--- airbnb/app/components/Navbar/Search.tsx
+++ airbnb/app/components/Navbar/Search.tsx
@@ -1,10 +1,56 @@
-'use client'
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import { useMemo } from 'react';
+import { BiSearch } from 'react-icons/bi';
+import { differenceInDays } from 'date-fns';
+
 import useSearchModal from '@/app/hooks/useSearchModal';
-import {BiSearch} from 'react-icons/bi'
+import useCountries from '@/app//hooks/UseCountries';
 
 const Search = () => {
-  const searchModal = useSearchModal()
-return ( 
+  const searchModal = useSearchModal();
+  const params = useSearchParams();
+  const { getByValue } = useCountries();
+
+  const  locationValue = params?.get('locationValue'); 
+  const  startDate = params?.get('startDate');
+  const  endDate = params?.get('endDate');
+  const  guestCount = params?.get('guestCount');
+
+  const locationLabel = useMemo(() => {
+    if (locationValue) {
+      return getByValue(locationValue as string)?.label;
+    }
+
+    return 'Anywhere';
+  }, [locationValue, getByValue]);
+
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate as string);
+      const end = new Date(endDate as string);
+      let diff = differenceInDays(end, start);
+
+      if (diff === 0) {
+        diff = 1;
+      }
+
+      return `${diff} Days`;
+    }
+
+    return 'Any Week'
+  }, [startDate, endDate]);
+
+  const guestLabel = useMemo(() => {
+    if (guestCount) {
+      return `${guestCount} Guests`;
+    }
+
+    return 'Add Guests';
+  }, [guestCount]);
+
+  return ( 
     <div
       onClick={searchModal.onOpen}
       className="
@@ -34,7 +80,7 @@ return (
             px-6
           "
         >
-          Anywhere
+          {locationLabel}
         </div>
         <div 
           className="
@@ -48,7 +94,7 @@ return (
             text-center
           "
         >
-         Anyweek
+          {durationLabel}
         </div>
         <div 
           className="
@@ -62,7 +108,7 @@ return (
             gap-3
           "
         >
-          <div className="hidden sm:block">Add Guests</div>
+          <div className="hidden sm:block">{guestLabel}</div>
           <div 
             className="
               p-2 
@@ -78,5 +124,5 @@ return (
     </div>
   );
 }
-
-export default Search
+ 
+export default Search;
